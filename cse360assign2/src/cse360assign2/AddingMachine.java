1f6e6d11b94ca39aac2c9e946832af3ccf564a63
zhenghang <zhenghang@DESKTOP-HHCVKMT>
2019-06-18 20:52:40 -0700

    initial version

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,48 @@
+/**Assignment #: 2
+* @author Hang Zheng	
+* Lecture: T/Th 11:00
+* this class create and calculate the total value
+* of the machine.
+*/
+
+package cse360assign2;
+
+public class AddingMachine {
+private int total;
+	/**
+	 * initialize total
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * 
+	 * @return return the total value
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 * this method add the machine to total value 
+	 */
+	public void add (int value) {
+		
+	}
+	/**
+	 * this method subtract the machine to total value 
+	 */
+	public void subtract (int value) {
+		
+	}
+	/**
+	 * 	(non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	public void clear() {
+	
+	}
+}
